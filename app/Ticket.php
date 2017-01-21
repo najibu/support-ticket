898df1eeb9e70bd60d19024c0f8e66a087079817
najibu <ns.najibu@gmail.com>
2017-01-21 20:25:20 +0000

    Added relationship

--- app/Ticket.php
+++ app/Ticket.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use App\Category;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
     protected $guarded = ['id'];
+
+    public function category()
+    {
+      return $this->belongsTo(Category::class);
+    }
 }
