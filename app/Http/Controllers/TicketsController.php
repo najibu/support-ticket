c2887741b351d32930676354db6d9603bb054d18
najibu <ns.najibu@gmail.com>
2017-01-22 15:06:48 +0000

    Added close action

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -71,4 +71,19 @@ class TicketsController extends Controller
 
       return view('tickets.show', compact('ticket', 'category', 'comments'));
     }
+
+    public function close($ticket_id, AppMailer $mailer)
+    {
+      $ticket = Ticket::where('ticket_id', $ticket_id)->firstOrFail();
+
+      $ticket->status = 'Closed';
+
+      $ticket->save();
+
+      $ticketOwner = $ticket->user;
+
+      $mailer->sendTicketStatusNotification($ticketOwner, $ticket);
+
+      return redirect()->back()->with('status', 'The ticket has been closed');
+    }
 }
