0108617af844ec5f49a4156b21b417bf60478a5c
najibu <ns.najibu@gmail.com>
2017-01-22 15:11:16 +0000

    Allowing Only Authenticated Users Access

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Auth;
 
 class TicketsController extends Controller
 {
+    public function __construct()
+    {
+      $this->middleware('auth');
+    }
+    
     public function index()
     {
       $tickets = Ticket::paginate(10);
